57f4191a2678221c35d69c1c141c277a361bd682
Mirko Bellabarba <mirko.bellabarba@gmail.com>
2017-02-22 22:03:19 +0100

    Aggiunta navigazione al click di un elemento sulla lista

--- IsiiSports/ViewModels/GameViewModel.cs
+++ IsiiSports/ViewModels/GameViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace IsiiSports.ViewModels
+﻿using IsiiSports.DataObjects;
+
+namespace IsiiSports.ViewModels
 {
     public class GameViewModel : BaseViewModel
     {
+
+        #region Methods
+
+        public override void Init(object initData)
+        {
+            var game = (Game) initData;
+            base.CoreMethods.DisplayAlert("Arguments", $"{game?.Team1?.Name} vs {game?.Team2?.Name}", "Ok");
+            base.Init(initData);
+        }
+
+        #endregion
     }
 }
--- IsiiSports/ViewModels/GamesViewModel.cs
+++ IsiiSports/ViewModels/GamesViewModel.cs
@@ -2,14 +2,20 @@
 using MvvmHelpers;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace IsiiSports.ViewModels
 {
     public class GamesViewModel : BaseViewModel
     {
+        #region Properties
         public ObservableRangeCollection<Game> GameList { get; set; } = new ObservableRangeCollection<Game>();
+        #endregion
 
+        #region Methods
         public GamesViewModel()
         {
 
@@ -24,5 +30,17 @@ namespace IsiiSports.ViewModels
             var result = await AzureService.GameStore.GetItemsAsync();
             this.GameList.ReplaceRange(result);
         }
+        #endregion
+
+        #region Commands
+
+        private ICommand itemTappedCommand;
+        public ICommand ItemTappedCommand => itemTappedCommand ?? (itemTappedCommand = new Command(ExecuteItemTappedCommand));
+        private void ExecuteItemTappedCommand(object item)
+        {
+            base.CoreMethods.PushPageModel<GameViewModel>(item, false, true);
+        }
+
+        #endregion
     }
 }
