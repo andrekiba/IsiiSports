37f2d4e5c669f5084e504958f97a064577e10b13
Andrea <andre.kiba@gmail.com>
2017-03-14 21:28:44 +0100

    master

--- IsiiSports/ViewModels/LoginViewModel.cs
+++ IsiiSports/ViewModels/LoginViewModel.cs
@@ -13,6 +13,39 @@ namespace IsiiSports.ViewModels
     {
         #region Commands
 		       
+		private ICommand loginCommand;
+		public ICommand LoginCommand => loginCommand ?? (loginCommand = new Command<string>(async authProvider => await ExecuteLoginCommand(authProvider), authProvider => IsNotBusy));
+
+		private ICommand skipLoginCommand;
+		public ICommand SkipLoginCommand => skipLoginCommand ?? (skipLoginCommand = new Command(ExecuteSkipLoginCommand, () => IsNotBusy));
+
+		#endregion
+
+		#region Methods
+		private void ExecuteSkipLoginCommand()
+		{
+			CoreMethods.SwitchOutRootNavigation(NavigationContainerNames.MainContainer);
+		}
+
+		private async Task ExecuteLoginCommand(string authProvider)
+		{
+			if (IsBusy)
+				return;
+
+			IsBusy = true;
+
+			var loggedIn = await AzureService.LoginAsync(authProvider);
+
+			if (loggedIn)
+			{
+				MessagingCenter.Send(App.Instance, Messages.UserLoggedIn);
+				CoreMethods.SwitchOutRootNavigation(NavigationContainerNames.MainContainer);
+			}
+			else
+				await UserDialogs.Instance.AlertAsync("Errore durante il login...", "Error");
+
+			IsBusy = false;
+		}
 
         #endregion 
     }
